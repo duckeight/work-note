63e61a697eca68c7161a54a6997969ca8bd5f9a1
Yeongju Kim <yjkim@nextree.io>
2026-03-17 18:19:18 +0900

    update ddl, jmeter

--- dev/lakey/ddl.sql
+++ dev/lakey/ddl.sql
@@ -1,30 +1,3 @@
--- lakey.analysis_history definition
-
--- Drop table
-
--- DROP TABLE vizend_lakey_dev.analysis_history;
-CREATE TABLE vizend_lakey_dev.analysis_history (
-                                                   id varchar(255) NOT NULL,
-                                                   goal text NULL,
-                                                   retrieved_context text NULL,
-                                                   final_prompt text NULL,
-                                                   "result" text NULL,
-                                                   verified bool NOT NULL,
-                                                   analysis_job_execution_id varchar(255) NULL,
-                                                   subject_id varchar(255) NULL,
-                                                   execution_time int8 NOT NULL,
-                                                   model_name varchar(255) NULL,
-                                                   entity_version int8 NOT NULL,
-                                                   modified_by varchar(255) NULL,
-                                                   modified_on int8 NOT NULL,
-                                                   registered_by varchar(255) NULL,
-                                                   registered_on int8 NOT NULL,
-                                                   actor_id varchar(255) NULL,
-                                                   pavilion_id varchar(255) NULL,
-                                                   stage_id varchar(255) NULL,
-                                                   CONSTRAINT analysis_history_pkey PRIMARY KEY (id)
-);
-
 -- lakey.ANALYSIS_JOB definition
 
 -- Drop table
@@ -82,6 +55,32 @@ CREATE TABLE vizend_lakey_dev.analysis_job_execution (
                                                          CONSTRAINT analysis_job_execution_pkey PRIMARY KEY (id)
 );
 
+-- lakey.analysis_history definition
+
+-- Drop table
+
+-- DROP TABLE vizend_lakey_dev.analysis_history;
+CREATE TABLE vizend_lakey_dev.analysis_history (
+                                                   id varchar(255) NOT NULL,
+                                                   goal text NULL,
+                                                   retrieved_context text NULL,
+                                                   final_prompt text NULL,
+                                                   result_json text NULL,
+                                                   verified bool NOT NULL,
+                                                   analysis_job_execution_id varchar(255) NULL,
+                                                   subject_id varchar(255) NULL,
+                                                   execution_time int8 NOT NULL,
+                                                   model_name varchar(255) NULL,
+                                                   entity_version int8 NOT NULL,
+                                                   modified_by varchar(255) NULL,
+                                                   modified_on int8 NOT NULL,
+                                                   registered_by varchar(255) NULL,
+                                                   registered_on int8 NOT NULL,
+                                                   actor_id varchar(255) NULL,
+                                                   pavilion_id varchar(255) NULL,
+                                                   stage_id varchar(255) NULL,
+                                                   CONSTRAINT analysis_history_pkey PRIMARY KEY (id)
+);
 -- vizend_lakey_dev.analysis_result definition
 
 -- Drop table
@@ -90,7 +89,7 @@ CREATE TABLE vizend_lakey_dev.analysis_job_execution (
 
 CREATE TABLE vizend_lakey_dev.analysis_result (
                                                   id varchar(255) NOT NULL,
-                                                  "result" text NULL,
+                                                  result_json text NULL,
                                                   verified bool NOT NULL,
                                                   subject_id varchar(255) NULL,
                                                   analysis_job_execution_id varchar(255) NULL,
@@ -139,7 +138,6 @@ CREATE TABLE vizend_lakey_dev.timeline (
                                            CONSTRAINT timeline_pkey PRIMARY KEY (id)
 );
 CREATE INDEX timeline_event_id_idx ON vizend_lakey_dev.timeline USING btree (event_id);
-
 CREATE TABLE vizend_lakey_dev.timeline_summary (
                                                    id varchar(255) NOT NULL,
                                                    subject_type varchar(255) NULL,
@@ -148,7 +146,7 @@ CREATE TABLE vizend_lakey_dev.timeline_summary (
                                                    description text NULL,
                                                    summarized_from int8 NOT NULL,
                                                    summarized_to int8 NOT NULL,
-                                                   summary_period varchar(255) NULL,
+                                                   summary_period_json text NULL,
                                                    timeline_summary_job_id varchar(255) NULL,
                                                    entity_version int8 NOT NULL,
                                                    modified_by varchar(255) NULL,
@@ -167,7 +165,7 @@ CREATE TABLE vizend_lakey_dev.timeline_summary_job (
                                                        category varchar(255) NULL,
                                                        requested_query text NULL,
                                                        start_date date NULL,
-                                                       summary_period varchar(255) NULL,
+                                                       summary_period_json text NULL,
                                                        active bool NOT NULL,
                                                        entity_version int8 NOT NULL,
                                                        modified_by varchar(255) NULL,
@@ -194,6 +192,7 @@ CREATE TABLE vizend_lakey_dev.raw_event (
                                             occurred_at int8 NOT NULL,
                                             topic_name varchar(255) NULL,
                                             conversion_status varchar(255) NULL,
+                                            conversion_failed_message varchar(255) NULL,
                                             entity_version int8 NOT NULL,
                                             modified_by varchar(255) NULL,
                                             modified_on int8 NOT NULL,
@@ -224,4 +223,23 @@ CREATE TABLE vizend_lakey_dev.profile (
                                           pavilion_id varchar(255) NULL,
                                           stage_id varchar(255) NULL,
                                           CONSTRAINT profile_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE vizend_lakey_dev.profile_vector (
+                                                 id varchar(255) NULL,
+                                                 category varchar(255) NULL,
+                                                 description text NULL,
+                                                 profile_id varchar(255) NULL,
+                                                 related_subjects_json text NULL,
+                                                 vector _float4 NULL,
+                                                 entity_version int8 NOT NULL,
+                                                 modified_by varchar(255) NULL,
+                                                 modified_on int8 NOT NULL,
+                                                 registered_by varchar(255) NULL,
+                                                 registered_on int8 NOT NULL,
+                                                 actor_id varchar(255) NULL,
+                                                 pavilion_id varchar(255) NULL,
+                                                 stage_id varchar(255) NULL,
+                                                 CONSTRAINT profile_vector_pkey PRIMARY KEY (id)
 );
